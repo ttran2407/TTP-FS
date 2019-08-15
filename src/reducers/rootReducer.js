19a374aba248517b435be66a737877f241e47607
github username <github email address>
2019-08-15 00:57:42 -0400

    set up Redux

---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+
+}
+
+
+// ---------------- REDUCER -----------------------
+
+const rootReducer = (state = initialState, action) => {
+    switch(action.type){
+        
+    }
+}
